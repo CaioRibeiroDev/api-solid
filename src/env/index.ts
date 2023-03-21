f5cbeb89c61a5ae8b83fa11198ceb6e62615780f
CaioRibeiroDev <caioribeirodev@gmail.com>
2023-03-21 11:10:23 -0300

    🎉 begin project

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+// process.env: { NODE_ENV: 'dev, ... }
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333)
+})
+
+const _env = envSchema.safeParse(process.env) // Validation
+
+if (_env.success === false) {
+  console.error('❌ invalid environment variavles', _env.error.format())
+
+  throw new Error(' invalid environment variavles')
+}
+
+export const env = _env.data
