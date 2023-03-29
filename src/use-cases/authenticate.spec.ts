769fec07a6984858db01b49b1d542db7f312d556
CaioRibeiroDev <caioribeirodev@gmail.com>
2023-03-29 18:06:47 -0300

    Authenticate test

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,14 +1,14 @@
-import { inMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { hash } from 'bcryptjs'
 import { beforeEach, describe, expect, it } from 'vitest'
 import { AuthenticateUseCase } from './authenticate'
 import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
-let usersRepository: inMemoryUsersRepository
+let usersRepository: InMemoryUsersRepository
 let sut : AuthenticateUseCase
 describe('Authenticate Use Case', () => {
   beforeEach(() => {
-    usersRepository = new inMemoryUsersRepository()
+    usersRepository = new InMemoryUsersRepository()
     sut = new AuthenticateUseCase(usersRepository)
   })
 
