5b89632d79c11077bf99c5a2c767e70b28a52543
CaioRibeiroDev <caioribeirodev@gmail.com>
2023-03-31 13:26:10 -0300

    Test  environment of prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,40 @@
+import 'dotenv/config'
+import { execSync } from 'node:child_process';
+import { PrismaClient } from '@prisma/client';
+
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if(!process.env.DATABASE_URL) throw new Error("Please provide a DATABASE_URL environment variable")
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment> {
   name: 'prisma',
   async setup() { //Executa antes de cada arquivo dos test
-    console.log('setup')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    // Tudo que passar dentro dessa função é como se estivesse executando o comando no terminal
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() { //executa apos os testes executarem (para cada arquivo de test)
-        console.log('teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       } 
     }
   }
