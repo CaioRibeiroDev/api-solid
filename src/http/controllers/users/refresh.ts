7542a4a93399498ee72085371d8e95ee620e89c1
CaioRibeiroDev <caioribeirodev@gmail.com>
2023-04-03 09:48:15 -0300

    authorization for roles

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -3,13 +3,16 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify({ onlyCookie: true})
 
-  const token = await reply.jwtSign({}, {
+  const { role } = request.user
+
+  const token = await reply.jwtSign({role}, {
     sign: {
       sub: request.user.sub
     }
   })
 
-  const refresehToken = await reply.jwtSign({}, {
+  const refresehToken = await reply.jwtSign(
+  {role}, {
     sign: {
       sub:  request.user.sub,
       expiresIn: '7d'
